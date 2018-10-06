2507964e4e8f2c02fcad8daa246621c5b8776ef1
Vincent Riva <treast@macbook-pro-de-vincent.home>
2018-10-06 11:04:00 +0200

    Add camera travelling

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -3,15 +3,61 @@ import SceneManager from "./utils/SceneManager";
 import ObjectLoader from './utils/ObjectLoader'
 
 let app = new App();
+let tubeGeometry, binormal = new THREE.Vector3(), normal = new THREE.Vector3();
 
 app.isReady().then(() => {
   let sceneManager = new SceneManager()
-  ObjectLoader.loadCollada('./assets/Trump.dae')
+  ObjectLoader.loadCollada('./assets/Trump10.dae')
     .then(collada => {
+      console.log(collada.scene)
       sceneManager.addObjectToScene(collada.scene)
+      let position = sceneManager.getObject("Spline").geometry.getAttribute('position')
+      let vertices = []
+      let scale = 1;
+      for(let i = 0; i < position.count; i += 3) {
+        vertices.push(new THREE.Vector3(position.array[i] / scale, position.array[i + 1] / scale, position.array[i + 2] / scale))
+      }
+      //sceneManager.camera.position.set(position.array[0], position.array[1], position.array[2])
+      let curve = new THREE.CatmullRomCurve3(vertices)
+      let points = curve.getPoints(200)
+      tubeGeometry = new THREE.TubeBufferGeometry(curve, 6, 1, 8, true)
+
     })
 
-  sceneManager.animate(() => {
+  /*ObjectLoader.loadObjectWithMaterial('./assets/Trump.obj', './assets/Trump.mtl')
+    .then(object => {
+      console.log(object)
+      sceneManager.addObjectToScene(object)
+    }) */
 
+  sceneManager.animate(() => {
+    let time = Date.now()
+    let loopTime = 10 * 1000
+    let t = (time % loopTime) / loopTime
+
+    if(tubeGeometry && true) {
+      let segments = tubeGeometry.tangents.length
+      let pickt = t * segments
+      let pick = Math.floor(pickt)
+      let pickNext = (pick + 1) % segments
+      let pos = tubeGeometry.parameters.path.getPointAt(t)
+
+      binormal.subVectors(tubeGeometry.binormals[pickNext], tubeGeometry.binormals[pick])
+      binormal.multiplyScalar(pickt - pick).add(tubeGeometry.binormals[pick])
+      let dir = tubeGeometry.parameters.path.getTangentAt(t)
+      let offset = 15
+
+      normal.copy(binormal).cross(dir)
+      pos.add(normal.clone().multiplyScalar(offset))
+      sceneManager.camera.position.copy(pos)
+
+      let lookAt = tubeGeometry.parameters.path.getPointAt((t + 30 / tubeGeometry.parameters.path.getLength()) % 1)
+      console.log(pos)
+      console.log(sceneManager.camera.position)
+      lookAt.copy(pos).add(dir)
+      sceneManager.camera.matrix.lookAt(sceneManager.camera.position, lookAt, normal)
+      sceneManager.camera.rotation.setFromRotationMatrix(sceneManager.camera.matrix, sceneManager.camera.rotation.order)
+      sceneManager.cameraHelper.update()
+    }
   })
 });
--- src/app/js/utils/SceneManager.js
+++ src/app/js/utils/SceneManager.js
@@ -28,8 +28,11 @@ export default class SceneManager {
   createCamera() {
     this.camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000)
     this.camera.position.set(-8.1, 3.1, 1.9)
-    this.camera.lookAt(10, 115, -80)
-    this.controls = new THREE.OrbitControls(this.camera)
+    this.camera.lookAt(0.1, 1, -0.8)
+    this.cameraHelper = new THREE.CameraHelper(this.camera)
+    this.scene.add(this.camera)
+    this.scene.add(this.cameraHelper)
+    //this.controls = new THREE.OrbitControls(this.camera)
   }
 
   createRenderer() {
@@ -40,12 +43,16 @@ export default class SceneManager {
 
   animate(animation = () => {}) {
     requestAnimationFrame(() => {this.animate(animation)})
-    this.controls.update()
+    //this.controls.update()
     this.renderer.render(this.scene, this.camera)
     animation();
   }
 
   addObjectToScene(object) {
     this.scene.add(object)
+  }
+
+  getObject(name) {
+    return this.scene.getObjectByName(name, true)
   }
 }
